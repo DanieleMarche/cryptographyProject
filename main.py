f1a97ac448fb77abf3b85384ba3d33c08253f55c
Daniele Marchetilli <marchetilli@gmail.com>
2024-10-17 15:24:49 +0200

    begin working on log screen

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-print("Hello World!")
+import tkinter as tk
+
+import Models.user_model
+from Controllers.LoginController import *
+from Models.user_model import UserModel
+from Views.log_in_page_view import LoginView
+
+if __name__ == "__main__":
+    root = tk.Tk()
+
+    # Creates the view
+    view = LoginView(root)
+
+    # Creates controller with the view
+    model = UserModel()
+
+    controller = LoginController(view, model)
+
+
+    # Esecuzione della finestra
+    root.mainloop()
 
